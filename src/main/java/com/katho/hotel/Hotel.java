005018ee09677783b7446a782bce5456b216f449
paulokatho <pkatho@gmail.com>
2023-11-22 00:23:40 +0000

    feat: Se aplicado, esse commit vai criar a classe Hotel

---
+++ src/main/java/com/katho/hotel/Hotel.java
@@ -0,0 +1,11 @@
+package com.katho.hotel;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Hotel extends PanacheEntity {
+
+    public Long travelOrderId;
+    public Integer nights;
+}
