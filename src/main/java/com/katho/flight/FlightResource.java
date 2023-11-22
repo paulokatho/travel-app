862eb7234269451d8c67c7f2f4f2256be073fd11
paulokatho <pkatho@gmail.com>
2023-11-22 00:23:27 +0000

    feat: Se aplicado, esse commit vai criar a classe FlightResource

---
+++ src/main/java/com/katho/flight/FlightResource.java
@@ -0,0 +1,36 @@
+package com.katho.flight;
+
+import jakarta.persistence.Entity;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("flight")
+public class FlightResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Flight> flights() {
+        return Flight.listAll();
+    }
+
+    @GET
+    @Path("findById")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Flight findById(@QueryParam("id") long id) {
+        return Flight.findById(id);
+    }
+
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Flight newFlight(Flight flight) {
+        flight.id = null;
+        flight.persist();
+
+        return flight;
+    }
+}
