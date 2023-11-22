ac911c39a82859d4320ab71150b79b6712c0a0aa
paulokatho <pkatho@gmail.com>
2023-11-22 00:23:50 +0000

    feat: Se aplicado, esse commit vai criar a classe HotelResource

---
+++ src/main/java/com/katho/hotel/HotelResource.java
@@ -0,0 +1,35 @@
+package com.katho.hotel;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("hotel")
+public class HotelResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Hotel> listAll() {
+        return Hotel.listAll();
+    }
+
+    @GET
+    @Path("findById")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Hotel findById(@QueryParam("id") long id) {
+        return Hotel.findById(id);
+    }
+
+    @POST
+    @Transactional
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Hotel newHotel(Hotel hotel) {
+        hotel.id = null;
+        hotel.persist();
+
+        return hotel;
+    }
+}
